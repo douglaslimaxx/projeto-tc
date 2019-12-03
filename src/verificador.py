d7fa47c9b263d8637e9844c8aa4fb408d9d28079
douglaslimaxx <douglas.lima@ccc.ufcg.edu.br>
2019-12-03 00:12:54 -0300

    finalizado

--- src/verificador.py
+++ src/verificador.py
@@ -3,21 +3,23 @@ import sys
 from leitor import importa_automato
 
 def verifica(automato, palavra):
-    estado_atual = automato.estado_inicial
+    estado_atual = [automato.estado_inicial]
     print("Estado        Palavra")
     for i in range(len(palavra)):
         print("%s             %s" % (estado_atual, palavra[i:len(palavra)]))
         estado_atual = efetua_transicao(palavra[i], automato, estado_atual)
     print("%s             %s" % (estado_atual, "e"))
-    if(estado_atual in automato.estados_aceitacao):
-        return True
-    else:
-        return False
-
+    for k in estado_atual:
+        if (k in automato.estados_aceitacao):
+            return True
+    return False
 def efetua_transicao(terminal, automato, estado_atual):
+    destinos = []
     for i in automato.transicoes:
-        if((estado_atual == i.origem) and (terminal == i.entrada)):
-            return i.destino
+        for j in estado_atual:
+            if((j == i.origem) and ((terminal == i.entrada) or i.entrada == "e")):
+                destinos.append(i.destino)
+    return destinos
 
 def verificador(automato, palavra):
     resultado = verifica(automato, palavra)
