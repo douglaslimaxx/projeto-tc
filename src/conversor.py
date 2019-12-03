f2c912254c6dcb2ee0aa9fcd26f88026b7b4fdd4
LukeHxH <lucasmed812@gmail.com>
2019-12-02 23:40:27 -0300

    Adiciona exemplo para conversor e modifica o arquivo

--- src/conversor.py
+++ src/conversor.py
@@ -6,24 +6,11 @@ from leitor import *
 args = sys.argv[1:]
 caminho = args[0]
 automato = importa_automato(caminho)
-afn = AutomatoNaoDeterministico(automato.estados, automato.estado_inicial, automato.estados_aceitacao, automato.transicoes)
+afn = AutomatoNaoDeterministico(
+    automato.estados, automato.estado_inicial, automato.estados_aceitacao, automato.transicoes)
 
-print("NÃO DETERMINÍSTICO: ")
-print("Estados:", afn.estados)
-print("Estado inicial:", afn.estado_inicial)
-print("Estados de aceitação:", afn.estados_aceitacao)
-print("Transições:")
-for transicao in afn.transicoes:
-    print(transicao)
-
+imprimir_automato(afn)
 afd = AutomatoDeterministico()
-
 afd.converter_a_partir_de_afn(afn)
 
-print("DETERMINÍSTICO")
-print("Estados:", afd.estados)
-print("Estado inicial:", afd.estado_inicial)
-print("Estados de aceitação:", afd.estados_aceitacao)
-print("Transições:")
-for transicao in afd.transicoes:
-    print(transicao)
+imprimir_automato(afd)
