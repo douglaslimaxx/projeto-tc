fc7bc88f013d313b82816776bc5bc5084d178e47
Eduardoccufcg <eduardo.pereira.santos@ccc.ufcg.edu.br>
2019-12-02 23:52:15 -0300

    Testes

--- src/testes.py
+++ src/testes.py
@@ -1,43 +1,10 @@
 import unittest
 from classes import *
 from operacoes import *
-from verificador import *
+
 
 
 class Test(unittest.TestCase):
-
-    def test_aceita_afd(self):
-        automato1 = Automato(["A", "B"], "A", ["B"], [Transicao(
-            "A", "0", "B"), Transicao("A", "1", "A"), Transicao("B", "1", "B"), Transicao("B", "0", "A")])
-
-        self.assertEqual(verifica(automato1, "101"), True)
-
-    def test_rejeita_afn(self):
-        automato1 = Automato(["A", "B"], "A", ["B"], [Transicao(
-            "A", "0", "A"), Transicao("A", "1", "A"), Transicao("A", "1", "B")])
-        self.assertEqual(verifica(automato1, "1001000"), 0)
-
-    def test_aceita_afn(self):
-        automato1 = Automato(["A", "B"], "A", ["B"], [Transicao(
-            "A", "0", "A"), Transicao("A", "1", "A"), Transicao("A", "1", "B")])
-
-        self.assertEqual(verifica(automato1, "1001"), 1)
-    """
-    0*1*
-    """
-
-    def test_aceita_afn_e(self):
-        automato1 = Automato(["A", "B"], "A", ["B"], [Transicao(
-            "A", "0", "A"), Transicao("A", "e", "B"), Transicao("B", "1", "B")])
-
-        self.assertEqual(verifica(automato1, "0000011111"), 1)
-
-    def test_rejeita_afn_e(self):
-        automato1 = Automato(["A", "B"], "A", ["B"], [Transicao(
-            "A", "0", "A"), Transicao("A", "e", "B"), Transicao("B", "1", "B")])
-
-        self.assertEqual(verifica(automato1, "1111111011"), 0)
-        self.assertEqual(verifica(automato1, "00111000"), 0)
 
     def test_complemento(self):
         automato1 = Automato(["A", "B"], "A", ["B"], [Transicao(
